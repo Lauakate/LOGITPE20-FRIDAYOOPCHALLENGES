fdcab2104eb040569af7ea13602db15218e184a9
Cloth <kevin0q0q0q0qrix>
2021-01-28 00:02:46 +0200

    OOP

---
+++ FridayOOPChallenges/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace FridayOOPChallenges
+{
+    class Program
+    {
+        class Cat
+        {
+            string name;
+            string color;
+            double hunger;
+
+            public Cat(string _name, string _color)
+            {
+                name = _name;
+                color = _color;
+                hunger = 0;
+
+            }
+
+            public string Name
+            {
+                get { return name; }
+            }
+            public void Sleep()
+            {
+                Console.WriteLine("Cat sleeps!");
+                hunger += 0.2;
+            }
+            public void Meow()
+            {
+                Console.WriteLine("Meow!");
+            }
+
+            public double Hunger
+            {
+                get { return hunger; }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Cat newCat = new Cat("dots", "brown");
+            Console.WriteLine($"Hunger level: {newCat.Hunger}");
+
+            while (newCat.Hunger != 1)
+            {
+                newCat.Sleep();
+            }
+            Console.WriteLine($"Hunger level: {newCat.Hunger}");
+            newCat.Meow();
+        }
+    }
+}
---
+++ FridayOOPChallenges2/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace FridayOOPChallenges2
+{
+    class Program
+    {
+        class Pet
+        {
+            string name;
+            double age;
+            double weight;
+            int lifespan;
+
+            public Pet(string _name, double _weight)
+            {
+                name = _name;
+                weight = 0;
+                age = 0;
+                lifespan = 0;
+
+            }
+            public string Name
+            {
+                get { return Name; }
+            }
+
+            public int Age
+            {
+                get { return Age; }
+            }
+
+            public double Weight
+            {
+                get { return weight; }
+            }
+
+            public int Lifespan
+            {
+                get { return lifespan; }
+            }
+            public void PetInfo()
+            {
+                Console.WriteLine($"Name: {name} Age: {age}");
+            }
+
+            public void Lifespan1()
+            {
+                Console.WriteLine($"Lifespan: {lifespan}");
+            }
+
+            public void Weight1()
+            {
+                Console.WriteLine($"Lifespan: {weight}");
+            }
+
+            public void Eat()
+            {
+                weight += 0.2;
+                Console.WriteLine($"{name} munches!");
+            }
+
+            public void Exercise()
+            {
+                weight -= 0.1;
+                Console.WriteLine($"{name} exercises!");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Pet Hamster = new Pet("Hammy", 0);
+
+            for (int i = 0; i < 11; i++)
+            {
+                if (Hamster.Weight > 0.7)
+                {
+                    Hamster.Exercise();
+                    Console.WriteLine($"Hammy exercises!");
+                }
+                else
+                {
+                    Hamster.Eat();
+
+                }
+
+            }
+
+            Console.WriteLine("Hammy has passed away! :(");
+        }
+    }
+}
